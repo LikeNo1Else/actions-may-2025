a73a1996bb4af3e6bf68c9979e4172ee462530c0
LikeNo1Else <socratez.hf@gmail.com>
2025-08-06 23:19:38 +0000

    terraform updated

--- main.tf
+++ main.tf
@@ -2,8 +2,15 @@ provider aws {
     region = "us-east-2"
 }
 
-
 resource aws_instance web {
     ami           = "ami-08ca1d1e465fbfe0c"
     instance_type = "t3.micro"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-adileta"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
